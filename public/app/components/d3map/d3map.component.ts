1edf4910129169b94481dd685ec2a52da427cf9f
Sandeep Bohra <Sandeep.bohra@3pillar.corp>
2017-01-19 14:03:48 +0530

    DV-29 apply circle on heatmap

--- public/app/components/d3map/d3map.component.ts
+++ public/app/components/d3map/d3map.component.ts
@@ -7,63 +7,69 @@ import { ConfigService } from './../../services/config.service';
     moduleId: module.id,
     selector: 'd3-map',
     template: ``,
-    inputs: ['startdate','enddate','selectedRegion'],
-	outputs: ['isloading','selectRegion']
+    inputs: ['startdate', 'enddate', 'selectedRegion'],
+    outputs: ['isloading', 'selectRegion']
 })
 
-export class D3mapComponent implements OnInit, OnChanges{
+export class D3mapComponent implements OnInit, OnChanges {
     private startdate: string;
     private enddate: string;
     private isloading = new EventEmitter();
     private parentNativeElement: any;
-    private margin = {top: 20, right: 50, bottom: 30, left: 50};
+    private margin = { top: 20, right: 50, bottom: 30, left: 50 };
     private width: number;
     private height: number;
     private rotate: number = 0;//60;
     private maxlat: number = 83;
     private svg: any;
+    private projection: any;
     private path: any;
-    private tooltipGroup: any;    
+    private tooltipGroup: any;
     private worldMapJson: string = "app/components/d3map/worldmap.json";
-    private company:string;
-    private selectionMap: any;  
+    private company: string;
+    private selectionMap: any;
 
     selectedRegion: string;
     selectRegion: EventEmitter<string> = new EventEmitter<string>();
 
-    constructor(private element: ElementRef, 
-                private _awsService:AwsdataService,
-                private _config:ConfigService) {
-        this.company=this._config.company;
+    colorRange = ["green", "red"];
+    legendText = ["Low Uses", "High Uses"];
+
+    constructor(private element: ElementRef,
+        private _awsService: AwsdataService,
+        private _config: ConfigService) {
+        this.company = this._config.company;
         this.parentNativeElement = element.nativeElement;
-        this.width = 700 - this.margin.left - this.margin.right ;
+        this.width = 700 - this.margin.left - this.margin.right;
         this.height = 400 - this.margin.top - this.margin.bottom;
     }
 
-    ngOnInit(): void{
+    ngOnInit(): void {
         this.initSvg();
+        this.getProjection();
         this.getPath();
-        this.getTooltip();     
+        this.getTooltip();
+        this.getLegend();
     }
 
     ngOnChanges(): void {
         this.getRegionsData();
     }
 
-    getRegionsData(){
+    getRegionsData() {
         let awsdata = {
-    		company: this.company,
-    		strdate: this.startdate,
-    		enddate: this.enddate
-		};
-        this._awsService.getRegionsData(awsdata).subscribe((regionsData)=>{
+            company: this.company,
+            strdate: this.startdate,
+            enddate: this.enddate
+        };
+        this._awsService.getRegionsData(awsdata).subscribe((regionsData) => {
             this.drawMap(regionsData);
-            
+
         })
     }
 
-    initSvg(): void{
-         if (this.parentNativeElement !== null){
+    initSvg(): void {
+        if (this.parentNativeElement !== null) {
             this.svg = d3.select(this.parentNativeElement)
                 .append('svg')
                 .attr("width", this.width + this.margin.left + this.margin.right)
@@ -76,96 +82,144 @@ export class D3mapComponent implements OnInit, OnChanges{
         }
     }
 
-    getPath(): void{
-        let projection = d3.geoMercator()
-            .rotate([this.rotate,0])
+    getProjection(): void {
+        this.projection = d3.geoMercator()
+            .rotate([this.rotate, 0])
             .scale(1)
-            .translate([this.width/2, this.height/2]);
+            .translate([this.width / 2, this.height / 2]);
 
         // set up the scale extent and initial scale for the projection
-        var b = this.mercatorBounds(projection, this.maxlat),
-            s = this.width/(b[1][0]-b[0][0]),
-            scaleExtent = [s, 10*s];
+        var b = this.mercatorBounds(this.projection, this.maxlat),
+            s = this.width / (b[1][0] - b[0][0]),
+            scaleExtent = [s, 10 * s];
 
-        projection
+        this.projection
             .scale(scaleExtent[0]);
+    }
 
+    getPath(): void {
         this.path = d3.geoPath()
-                      .projection(projection);
+            .projection(this.projection);
     }
 
     // find the top left and bottom right of current projection
     mercatorBounds(projection, maxlat) {
         var yaw = projection.rotate()[0],
-            xymax = projection([-yaw+180-1e-6,-maxlat]),
-            xymin = projection([-yaw-180+1e-6, maxlat]);
-        
-        return [xymin,xymax];
+            xymax = projection([-yaw + 180 - 1e-6, -maxlat]),
+            xymin = projection([-yaw - 180 + 1e-6, maxlat]);
+
+        return [xymin, xymax];
     }
 
-    getTooltip(): void{
+    getTooltip(): void {
         this.tooltipGroup = d3.select(this.parentNativeElement)
-                              .append("div")
-                              .attr("class", "tooltipcss")
-                              .style("visibility", "hidden")
-                              .html("a simple tooltip");
+            .append("div")
+            .attr("class", "tooltipcss").style("opacity", 0);
     }
 
-    drawMap(data): void{
+    getLegend(): void {
+        var legend = d3.select(this.parentNativeElement).append("svg")
+            .attr("class", "legend")
+            .attr("width", 500)
+            .attr("height", 40)
+            .selectAll("g")
+            .data(this.colorRange)
+            .enter()
+            .append("g")
+            .attr("transform", function (d, i) { return "translate(" + ((i * 150) + 100) + ", 10)"; });
+
+        legend.append("rect")
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", function (d) { return d; });
+
+        legend.append("text")
+            .data(this.legendText)
+            .attr("x", 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .text(function (d) { return d; });
+
+    }
+
+    drawMap(data): void {
         d3.json(this.worldMapJson, (error, collection) => {
-            if(error) throw error;
+            if (error) throw error;
 
             this.selectionMap = this.svg.selectAll("path")
-                                .data(collection.features);
-            
-            var color = d3.scaleLinear<string>()
-                        .domain([0,data.maxval])
-                        .range(["green", "red"]);
-            
-            this.selectionMap.enter().append("path")
-                     .attr("class", (d) => { return "subunit " + d.id; })
-                     .attr("d", this.path);
+                .data(collection.features);
 
-             this.svg.selectAll("path")
-                    .attr("fill", (d) => {  
-                         if(data.hasOwnProperty(d.id)){
-                              //return data[d.id].color;
-                              return color(data[d.id].totalcost);
-                         }
-                         else
-                         {
-                             return "#ccc";
-                         }
-                         
-                     })
-                     .attr("stroke", "#fff")
-                     .on( "click", (d) => {
-                         this.isloading.emit(true);
-                         this.selectRegion.emit(d.id);
-                     })
-                     .on("mouseover", (d) => {
-                         let tooltext = d.properties.name+"<br/>"
-                                        +"Region : "+d.id+"<br/>";
-                         if(data.hasOwnProperty(d.id)){
-                             tooltext =  d.properties.name+"<br/>"
-                                         +"Region : "+data[d.id].name+"<br/>"
-                                         +"Total Resource : "+data[d.id].totalresource+"<br/>"
-                                         +"Total Cost : "+data[d.id].totalcost;
-                             return this.tooltipGroup
-                                    .html(tooltext)
-                                    .style("visibility", "visible");
-                        }
-                         
-                     })
-                     .on("mousemove", (d) => {
-                         return this.tooltipGroup
-                                    .style("top", (d3.event.pageY-80)+"px")
-                                    .style("left",(d3.event.pageX-100)+"px");
-                     })
-                     .on("mouseout", (d) => {
-                         return this.tooltipGroup.style("visibility", "hidden");
-                     })                         
+            let color = d3.scaleLinear<string>()
+                .domain([0, data.maxval])
+                .range(this.colorRange);
+
+            this.selectionMap.enter().append("path")
+                .attr("class", (d) => { return "subunit " + d.id; })
+                .attr("d", this.path)
+                .attr("fill", "#ccc")
+                .attr("stroke", "#fff");
+
+            this.svg.append("g")
+                .attr("class", "bubble")
+                .selectAll("circle")
+                .data(collection.features)
+                .enter().append("circle")
+                .attr("transform", (d) => {
+                    return "translate(" + this.path.centroid(d) + ")";
+                });
+
+            this.svg.selectAll("circle")
+                .attr("r", (d) => {
+                    if (data.hasOwnProperty(d.id)) {
+                        return 12;
+                    }
+                })
+                .attr("class", (d) => {
+                    if (data.hasOwnProperty(d.id)) {
+                        return "resource-region";
+                    }
+                })
+                .style("cursor", "pointer")
+                .attr("fill", (d) => {
+                    if (data.hasOwnProperty(d.id)) {
+                        return color(data[d.id].totalcost);
+                    }
+                })
+                .on("click", (d) => {
+                    this.isloading.emit(true);
+                    this.selectRegion.emit(d.id);
+                })
+                .on("mouseover", (d) => {
+                    let tooltext = d.properties.name + "<br/>"
+                        + "Region : " + d.id + "<br/>";
+                    if (data.hasOwnProperty(d.id)) {
+                        tooltext = d.properties.name + "<br/>"
+                            + "Region : " + data[d.id].name + "<br/>"
+                            + "Total Hours : " + data[d.id].totalresource + "<br/>"
+                            + "Total Cost : $" + data[d.id].totalcost;
+
+                    }
+                    this.tooltipGroup.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    this.tooltipGroup.html(tooltext)
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mousemove", (d) => {
+                    this.tooltipGroup
+                        .style("top", (d3.event.pageY - 80) + "px")
+                        .style("left", (d3.event.pageX - 100) + "px");
+                })
+                .on("mouseout", (d) => {
+                    this.tooltipGroup.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                });
+
         })
+
+
     }
-    
+
 }
--- public/app/components/datatable/datatable.component.ts
+++ public/app/components/datatable/datatable.component.ts
@@ -1,109 +1,109 @@
-import { Component,EventEmitter,OnChanges }  from '@angular/core';
+import { Component, EventEmitter, OnChanges } from '@angular/core';
 import { Http } from '@angular/http';
 import { AwsdataService } from './../../services/awsdata.service';
 import { ConfigService } from './../../services/config.service';
 
 @Component({
-	moduleId: module.id,
-	selector : 'aws-billing-datatable',
-	templateUrl: 'datatable.component.html',
-	styleUrls: ['datatable.component.css'],
-    inputs: ['startdate','enddate','selectedRegion'],
-    outputs:['isloading']
+    moduleId: module.id,
+    selector: 'aws-billing-datatable',
+    templateUrl: 'datatable.component.html',
+    styleUrls: ['datatable.component.css'],
+    inputs: ['startdate', 'enddate', 'selectedRegion'],
+    outputs: ['isloading']
 })
-export class DatatableComponent implements OnChanges{
-    startdate:any;
-	enddate:any;
-    company:string;
+export class DatatableComponent implements OnChanges {
+    startdate: any;
+    enddate: any;
+    company: string;
     selectedRegion: string;
 
-    isloading=new EventEmitter();
-    
-    public data: any[]=[];
+    isloading = new EventEmitter();
+
+    public data: any[] = [];
     public filterQuery = "";
     public rowsOnPage = 10;
     public sortBy = "blandedcost";
     public sortOrder = "asc";
 
-    public currentPage:number = 1;
-    public totalItems:number = 200; // total numbar of page not items 
-    public maxSize:number = 10; // max page size 
-    filter:string='';
+    public currentPage: number = 1;
+    public totalItems: number = 200; // total numbar of page not items 
+    public maxSize: number = 10; // max page size 
+    filter: string = '';
 
-    
 
-    constructor(private http: Http,private _awsdata:AwsdataService,private _config:ConfigService) {
-        this.company=this._config.company;
+
+    constructor(private http: Http, private _awsdata: AwsdataService, private _config: ConfigService) {
+        this.company = this._config.company;
     }
 
     ngOnChanges(): void {
-        let awsdata={
+        let awsdata = {
             company: this.company,
             strdate: this.startdate,
             enddate: this.enddate,
             currentpage: this.currentPage,
-            size:   this.rowsOnPage,
+            size: this.rowsOnPage,
             filter: this.filter,
             region: this.selectedRegion
         };
         this.getAllAwsResourcedata(awsdata);
     }
-    
-    getAllAwsResourcedata(awsdata:any){
-        var jsondata:any=[];
-        this._awsdata.getAllAwsResource(awsdata).subscribe((data)=>{
-           if(data.hits.total){
-               
-               this.totalItems=data.hits.total;
-            
-           }
-           for(let hit of data.hits.hits){
+
+    getAllAwsResourcedata(awsdata: any) {
+        var jsondata: any = [];
+        this._awsdata.getAllAwsResource(awsdata).subscribe((data) => {
+            if (data.hits.total) {
+
+                this.totalItems = data.hits.total;
+
+            }
+            for (let hit of data.hits.hits) {
                 jsondata.push(hit._source);
             }
-            
-            this.data =jsondata;
+
+            this.data = jsondata;
             this.isloading.emit(false);
         });
 
 
     }
 
-    filterbyOperation(val:string){
-        this.filter=val;
-        this.currentPage=1;
-        let awsdata={
+    filterbyOperation(val: string) {
+        this.filter = val;
+        this.currentPage = 1;
+        let awsdata = {
             company: this.company,
             strdate: this.startdate,
             enddate: this.enddate,
             currentpage: this.currentPage,
-            size:   this.rowsOnPage,
+            size: this.rowsOnPage,
             filter: this.filter,
             region: this.selectedRegion
         };
         this.isloading.emit(true);
         this.getAllAwsResourcedata(awsdata);
-        
+
     }
 
-   
-   
-    public setPage(pageNo:number):void {
-        this.currentPage = pageNo;    
+
+
+    public setPage(pageNo: number): void {
+        this.currentPage = pageNo;
     };
 
-    public pageChanged(event:any):void {
+    public pageChanged(event: any): void {
         //this method will trigger every page click 
-        let awsdata={
+        let awsdata = {
             company: this.company,
             strdate: this.startdate,
             enddate: this.enddate,
             currentpage: this.currentPage,
-            size:   this.rowsOnPage,
+            size: this.rowsOnPage,
             filter: this.filter,
             region: this.selectedRegion
         };
         this.isloading.emit(true);
-        this.currentPage= event.itemsPerPage;
+        this.currentPage = event.itemsPerPage;
         this.getAllAwsResourcedata(awsdata);
     };
 
