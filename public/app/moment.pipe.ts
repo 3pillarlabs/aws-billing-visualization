fc75079a6fbec4e5cd5230e3b3a3551a3de51761
mahesh.chand <mahesh.chand@3pillarglobal.com>
2017-03-06 19:01:55 +0530

    AWS billing fixing dates in detailed report, Small format change to date in header

--- public/app/moment.pipe.ts
+++ public/app/moment.pipe.ts
@@ -3,6 +3,6 @@ declare var moment: any;
 @Pipe({name: 'moment'})
 export class MomentPipe implements PipeTransform {
   transform(value: string, format: string): string {
-    return moment(value, "YYYY-MM-DD").format(format);
+    return moment(value, "YYYY-MM-DD h:m:s").format(format);
   }
 }
