26a5e4c17945a83626a32f63db1e8e4037c4f09f
sndpbohra <sndpbohra@gmail.com>
2017-03-08 14:15:13 +0530

    Bar chart tooltip and selection from label

--- public/app/components/chart/barchart.component.ts
+++ public/app/components/chart/barchart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, AfterViewInit,ElementRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, ElementRef } from '@angular/core';
 import { AwsdataService } from './../../services/awsdata.service';
 import { ConfigService } from './../../services/config.service';
 import * as D3 from 'd3';
@@ -37,7 +37,7 @@ export class BarchartComponent implements AfterViewInit {
 
     private width;
     private height;
-    private margin = {top: 10, right: 40, bottom:80 , left: 60};
+    private margin = { top: 10, right: 40, bottom: 100, left: 60 };
     private xScale;
     private yScale;
     private dollarFormatter;
@@ -45,14 +45,15 @@ export class BarchartComponent implements AfterViewInit {
     private xAxis;
     public dataset = [];
     private svg;
-    private barheight:number=20;
+    private barheight: number = 20;
     private color;
     private tooltip;
     private y1Scale;
     private yAxisRight;
     private percentFormatter;
+    productDataSet = {};
 
-    constructor(private element: ElementRef,private _awsdata: AwsdataService, private _config: ConfigService) {
+    constructor(private element: ElementRef, private _awsdata: AwsdataService, private _config: ConfigService) {
         this.company = this._config.company;
     }
 
@@ -70,21 +71,35 @@ export class BarchartComponent implements AfterViewInit {
         this.setup();
         this.buildSVG();
     }
-    
+
     parsePieChartData(data: any): void {
         if (data && data.aggregations) {
             if (data.aggregations.product_name) {
                 let productdata = [];
                 for (let product of data.aggregations.product_name.buckets) {
-                    let TotalBlendedCost = Math.round(product.TotalBlendedCost.value)
+                    let TotalBlendedCost = Math.round(product.TotalBlendedCost.value);
+                    let originalCost = (product.TotalBlendedCost.value).toFixed(2);
                     if (TotalBlendedCost > 0) {
-                        var productname=product.key.replace(/AWS |Amazon /gi,"");
+                        var productname = product.key.replace(/AWS |Amazon /gi, "");
+                        let productTag = productname.replace(/[ ()]/g, '');
                         var productdoc = {
                             'name': productname,
                             'totalcost': TotalBlendedCost,
                             'totalresource': product.doc_count,
-                            'orignalname':product.key
+                            'orignalname': product.key,
+                            'producttag': productTag,
+                            'originalCost': originalCost
                         };
+
+                        this.productDataSet[productname] = {
+                            'name': productname,
+                            'totalcost': TotalBlendedCost,
+                            'totalresource': product.doc_count,
+                            'orignalname': product.key,
+                            'producttag': productTag,
+                            'originalCost': originalCost
+                        }
+
                         productdata.push(productdoc);
 
                     }
@@ -96,7 +111,7 @@ export class BarchartComponent implements AfterViewInit {
         }
     }
 
-  
+
     public getProduct(awsdata: any) {
         this._awsdata.getUniqueProduct(awsdata).subscribe((data) => {
             this.parsePieChartData(data);
@@ -106,35 +121,35 @@ export class BarchartComponent implements AfterViewInit {
     }
 
 
-    setup():void{
+    setup(): void {
         D3.select(this.element.nativeElement.querySelector('svg#awsbillingbarchart')).html("");
-        
+
         this.width = parseInt(D3.select(this.element.nativeElement.querySelector('div#awsbillingbarchartcontainer')).style("width")) - this.margin.left - this.margin.right,
-        this.height = parseInt(D3.select(this.element.nativeElement.querySelector('div#awsbillingbarchartcontainer')).style("height")) - this.margin.top - this.margin.bottom;
+            this.height = parseInt(D3.select(this.element.nativeElement.querySelector('div#awsbillingbarchartcontainer')).style("height")) - this.margin.top - this.margin.bottom;
 
         this.xScale = D3.scaleBand().range([0, this.width]).padding(0.1);
         this.yScale = D3.scaleLinear().range([this.height, 0]);
         this.y1Scale = D3.scaleLinear().range([this.height, 0]);
 
         this.dollarFormatter = D3.format(",.0f");
-        this.percentFormatter= D3.format(".0%");
-        
-        this.xAxis = D3.axisBottom(this.xScale);
-        this.yAxis = D3.axisLeft(this.yScale).tickFormat((d)=>{  return "$" + this.dollarFormatter(d); });
-        this.yAxisRight = D3.axisRight(this.y1Scale).tickFormat((d)=>{  return this.percentFormatter(d); });
+        this.percentFormatter = D3.format(".0%");
 
-        
+        this.xAxis = D3.axisBottom(this.xScale);
+        this.yAxis = D3.axisLeft(this.yScale).tickFormat((d) => { return "$" + this.dollarFormatter(d); });
+        this.yAxisRight = D3.axisRight(this.y1Scale).tickFormat((d) => { return this.percentFormatter(d); });
+
+
         this.color = D3.scaleOrdinal(D3.schemeCategory20);
-        this.tooltip= D3.select(this.element.nativeElement.querySelector('div#barcharttoolTip'));
+        this.tooltip = D3.select(this.element.nativeElement.querySelector('div#barcharttoolTip'));
     }
 
-    buildSVG():void{
-        let that=this;
+    buildSVG(): void {
+        let that = this;
         if (this.dataset.length > 0) {
-            var tots = D3.sum(this.dataset, function(d) { 
-                return d.totalcost; 
+            var tots = D3.sum(this.dataset, function (d) {
+                return d.totalcost;
             });
-            
+
             this.svg = D3.select(this.element.nativeElement.querySelector('svg#awsbillingbarchart'))
                 .attr("width", this.width + this.margin.left + this.margin.right)
                 .attr("height", this.height + this.margin.top + this.margin.bottom)
@@ -152,11 +167,12 @@ export class BarchartComponent implements AfterViewInit {
                 .enter().append("rect")
                 .attr("class", "bar")
                 .attr("rectClicked", "No")
-                .attr("x", (d,i)=> { return this.xScale(d.name); })
+                .attr("productAttrName", (d, i) => { return d.producttag; })
+                .attr("x", (d, i) => { return this.xScale(d.name); })
                 .attr("width", this.xScale.bandwidth())
-                .attr("y", (d,i)=> { return this.yScale(d.totalcost); })
-                .attr("height", (d)=> { return this.height - this.yScale(d.totalcost); })
-                .attr("fill",(d,i)=>{ return this.color(i)})
+                .attr("y", (d, i) => { return this.yScale(d.totalcost); })
+                .attr("height", (d) => { return this.height - this.yScale(d.totalcost); })
+                .attr("fill", (d, i) => { return this.color(i) })
                 .on("click", function (d, i) {
                     if (D3.select(this).attr("rectClicked") == "No") {
                         D3.selectAll("[rectClicked=Yes]")
@@ -165,13 +181,26 @@ export class BarchartComponent implements AfterViewInit {
                             .duration(500)
                             .attr("stroke", "none");
 
-                        D3.select(this)                            
+                        D3.select(this)
                             .attr("rectClicked", "Yes")
                             .transition()
                             .duration(500)
                             .attr("stroke", "black")
                             .attr("stroke-width", 2);
-                       that.selectProduct.emit(d.orignalname);
+
+                        D3.selectAll("[productClcked=Yes]")
+                            .attr("productClcked", "No")
+                            .transition()
+                            .duration(500)
+                            .style("font-weight", "normal");
+
+                        D3.selectAll("[productClcked=No]").filter("[txtProductAttrName=" + d.producttag + "]")
+                            .attr("productClcked", "Yes")
+                            .transition()
+                            .duration(500)
+                            .style("font-weight", "bold");
+
+                        that.selectProduct.emit(d.orignalname);
 
                     }
                     else if (D3.select(this).attr("rectClicked") == "Yes") {
@@ -181,35 +210,105 @@ export class BarchartComponent implements AfterViewInit {
                             .duration(500)
                             .attr("stroke", "none");
 
+                        D3.selectAll("[productClcked=Yes]").filter("[txtProductAttrName=" + d.producttag + "]")
+                            .attr("productClcked", "No")
+                            .transition()
+                            .duration(500)
+                            .style("font-weight", "normal");
+
                         that.selectProduct.emit("");
                     }
-                    
+
                 })
-                .on("mousemove", (d,i)=>{
+                .on("mousemove", (d, i) => {
                     this.tooltip
                         .style("left", D3.event.pageX - 50 + "px")
                         .style("top", D3.event.pageY - 100 + "px")
                         .style("display", "inline-block")
-                        .html((d.name) + "<br>" + "$" + (d.totalcost)+ "<br/>"+ Math.round((d.totalcost/tots)*100)+'%');
-                 })
-                .on("mouseout", (d)=>{ this.tooltip.style("display", "none");});
+                        .html((d.name) + "<br>" + "$" + (d.originalCost) + "<br/>" + Math.round((d.originalCost / tots) * 100) + '%');
+                })
+                .on("mouseout", (d) => { this.tooltip.style("display", "none"); });
 
             // add the x Axis
             this.svg.append("g")
                 .attr("transform", "translate(0," + this.height + ")")
                 .call(this.xAxis)
-                .selectAll("text")  
+                .selectAll("text")
+                .attr("productClcked", "No")
+                .attr("txtProductAttrName", (d, i) => { return this.productDataSet[d].producttag; })
                 .style("text-anchor", "end")
+                .style("cursor", "pointer")
                 .attr("dx", "-.8em")
                 .attr("dy", ".15em")
-                .attr("transform", "rotate(-35)" );
+                .attr("transform", "rotate(-40)")
+                .on("mousemove", (d, i) => {
+                    let toolname = "";
+                    if (this.productDataSet.hasOwnProperty(d)) {
+                        toolname = (d) + "<br>" + "$" + (this.productDataSet[d].originalCost) + "<br/>" + Math.round((this.productDataSet[d].originalCost / tots) * 100) + '%';
+                    } else {
+                        toolname = (d) + "<br>" + "$0" + "<br/>" + '0%';
+                    }
+                    this.tooltip
+                        .style("left", D3.event.pageX - 50 + "px")
+                        .style("top", D3.event.pageY - 100 + "px")
+                        .style("display", "inline-block")
+                        .html(toolname);
+                })
+                .on("mouseout", (d) => { this.tooltip.style("display", "none"); })
+                .on("click", function (d, i) {
+                    if (D3.select(this).attr("productClcked") == "No") {
+                        D3.selectAll("[productClcked=Yes]")
+                            .attr("productClcked", "No")
+                            .transition()
+                            .duration(500)
+                            .style("font-weight", "normal");
+
+                        D3.select(this)
+                            .attr("productClcked", "Yes")
+                            .transition()
+                            .duration(500)
+                            .style("font-weight", "bold");
+
+                        D3.selectAll("[rectClicked=Yes]")
+                            .attr("rectClicked", "No")
+                            .transition()
+                            .duration(500)
+                            .attr("stroke", "none");
+
+                        D3.selectAll("[rectClicked=No]").filter("[productAttrName=" + that.productDataSet[d].producttag + "]")
+                            .attr("rectClicked", "Yes")
+                            .transition()
+                            .duration(500)
+                            .attr("stroke", "black")
+                            .attr("stroke-width", 2);
+
+                        that.selectProduct.emit(that.productDataSet[d].orignalname);
+
+                    }
+                    else if (D3.select(this).attr("productClcked") == "Yes") {
+                        D3.select(this)
+                            .attr("productClcked", "No")
+                            .transition()
+                            .duration(500)
+                            .style("font-weight", "normal");
+
+                        D3.selectAll("[rectClicked=Yes]").filter("[productAttrName=" + that.productDataSet[d].producttag + "]")
+                            .attr("rectClicked", "No")
+                            .transition()
+                            .duration(500)
+                            .attr("stroke", "none");
+
+                        that.selectProduct.emit("");
+                    }
+
+                });
 
             // add the y Axis
             this.svg.append("g")
                 .call(this.yAxis);
 
             this.svg.append("g")
-                .attr("transform", "translate("+this.width+"," +"0)")
+                .attr("transform", "translate(" + this.width + "," + "0)")
                 .call(this.yAxisRight);
 
             /*this.svg.append("g")
@@ -226,12 +325,12 @@ export class BarchartComponent implements AfterViewInit {
                 .style("text-anchor", "middle")
                 .text("Sales");*/
 
-            
-        } 
+
+        }
     }
 
     BarChartResize(): void {
-        this.width = parseInt(D3.select(this.element.nativeElement.querySelector('svg#awsbillingbarchart')).style("width")) - this.margin.left - this.margin.right,
+        this.width = parseInt(D3.select(this.element.nativeElement.querySelector('svg#awsbillingbarchart')).style("width")) - this.margin.left - this.margin.right;
         this.height = parseInt(D3.select(this.element.nativeElement.querySelector('svg#awsbillingbarchart')).style("height")) - this.margin.top - this.margin.bottom;
 
         // Update the range of the scale with new width/height
