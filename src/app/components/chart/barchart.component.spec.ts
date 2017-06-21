82af0b1eea9d74dfcc2fe0b8c4a56a4f3b5b06dc
eashkhatri-3pg <eash.khatri@3pillarglobal.com>
2017-06-21 14:29:06 +0530

    comitting changes done so far

--- src/app/components/chart/barchart.component.spec.ts
+++ src/app/components/chart/barchart.component.spec.ts
@@ -5,7 +5,7 @@ import { BarchartComponent } from './barchart.component';
 import { HttpModule } from '@angular/http';
 import { AwsdataService } from './../../services/awsdata.service';
 
-describe('Barchart Component inline template',()=>{
+describe('Barchart Component', ()=>{
     let comp:BarchartComponent;
     let fixer:ComponentFixture<BarchartComponent>;
     let el: HTMLElement;
@@ -26,102 +26,116 @@ describe('Barchart Component inline template',()=>{
         let awsDataService = TestBed.get(AwsdataService);
     })
 
-    it('should have a defined component', () => {
-      expect(comp).toBeDefined();
-    });
-})
-
-
-describe('Parse pie chart data', ()=>{
-    let comp:BarchartComponent;
-    let fixer:ComponentFixture<BarchartComponent>;
-    let el: HTMLElement;
-    let del:DebugElement;
-    let awsdataServiceStub = {};
-    let defaultHtmlStringLength = 0;
-
-    beforeEach(()=>{
-        TestBed.configureTestingModule({
-            declarations:[BarchartComponent],
-            imports: [HttpModule],
-            providers:    [ {provide: AwsdataService, useValue: awsdataServiceStub } ],
-        })
-
-        fixer=TestBed.createComponent(BarchartComponent);
-        comp=fixer.componentInstance;
-
-        // AwsdataService from the root injector
-        let awsDataService = TestBed.get(AwsdataService);
-
-        // query for the title <h1> by CSS element selector
-        del = fixer.debugElement.query(By.css('div'));
-        el = del.nativeElement;
-        defaultHtmlStringLength = el.innerHTML.length;
+    describe('Barchart Component inline template',()=>{
+        it('should have a defined component', () => {
+          expect(comp).toBeDefined();
+        });
     })
 
+    describe('Parse pie chart data', ()=>{
+        let defaultHtmlStringLength = 0;
 
-    it('Check the SVG is rendered', ()=>{
+        beforeEach(()=>{
+            // query for the title <h1> by CSS element selector
+            del = fixer.debugElement.query(By.css('div'));
+            el = del.nativeElement;
+            defaultHtmlStringLength = el.innerHTML.length;
+        })
 
-      //el = document.getElementById('awsbillingbarchart');
-      var dummyData = {
-      	"took": 254,
-      	"timed_out": false,
-      	"_shards": {
-      		"total": 5,
-      		"successful": 5,
-      		"failed": 0
-      	},
-      	"hits": {
-      		"total": 1456932,
-      		"max_score": null,
-      		"hits": []
-      	},
-      	"aggregations": {
-      		"total_cost": {
-      			"value": 199760.4322595468
-      		},
-      		"product_name": {
-      			"doc_count_error_upper_bound": 0,
-      			"sum_other_doc_count": 14106,
-      			"buckets": [
-      				{
-      					"key": "Amazon Elastic Compute Cloud",
-      					"doc_count": 1398543,
-      					"TotalBlendedCost": {
-      						"value": 105656.82024444744
-      					}
-      				},
-      				{
-      					"key": "Amazon Elastic File System",
-      					"doc_count": 8846,
-      					"TotalBlendedCost": {
-      						"value": 46836.043028660264
-      					}
-      				}
-      			]
-      		},
-      		"AvailabilityRegion": {
-      			"doc_count_error_upper_bound": 0,
-      			"sum_other_doc_count": 0,
-      			"buckets": [
-      				{
-      					"key": "us-east-1",
-      					"doc_count": 277197,
-      					"TotalBlendedCost": {
-      						"value": 47676.985000011955
-      					}
-      				}
-      			]
-      		},
-      		"total_quantity": {
-      			"value": 144921900.4714016
-      		}
-      	}
-      };
+        it('Check the SVG is rendered', ()=>{
+          //el = document.getElementById('awsbillingbarchart');
+          var dummyData = {
+          	"took": 254,
+          	"timed_out": false,
+          	"_shards": {
+          		"total": 5,
+          		"successful": 5,
+          		"failed": 0
+          	},
+          	"hits": {
+          		"total": 1456932,
+          		"max_score": null,
+          		"hits": []
+          	},
+          	"aggregations": {
+          		"total_cost": {
+          			"value": 199760.4322595468
+          		},
+          		"product_name": {
+          			"doc_count_error_upper_bound": 0,
+          			"sum_other_doc_count": 14106,
+          			"buckets": [
+          				{
+          					"key": "Amazon Elastic Compute Cloud",
+          					"doc_count": 1398543,
+          					"TotalBlendedCost": {
+          						"value": 105656.82024444744
+          					}
+          				},
+          				{
+          					"key": "Amazon Elastic File System",
+          					"doc_count": 8846,
+          					"TotalBlendedCost": {
+          						"value": 46836.043028660264
+          					}
+          				}
+          			]
+          		},
+          		"AvailabilityRegion": {
+          			"doc_count_error_upper_bound": 0,
+          			"sum_other_doc_count": 0,
+          			"buckets": [
+          				{
+          					"key": "us-east-1",
+          					"doc_count": 277197,
+          					"TotalBlendedCost": {
+          						"value": 47676.985000011955
+          					}
+          				}
+          			]
+          		},
+          		"total_quantity": {
+          			"value": 144921900.4714016
+          		}
+          	}
+          };
 
-      comp.parsePieChartData(dummyData);
-      fixer.detectChanges();
-      expect(el.innerHTML.length).toBeGreaterThan(defaultHtmlStringLength);
+          comp.parsePieChartData(dummyData);
+          fixer.detectChanges();
+          expect(el.innerHTML.length).toBeGreaterThan(defaultHtmlStringLength);
 
+        })
+    })
+
+    describe('onResize', ()=>{
+          it('Should call setup', ()=>{
+              spyOn(comp, "setup");
+              comp.onResize();
+              expect(comp.setup).toHaveBeenCalled();
+          })
+
+          it('Should call buildSVG', ()=>{
+              spyOn(comp, "buildSVG");
+              comp.onResize();
+              expect(comp.buildSVG).toHaveBeenCalled();
+          })
+
+
+    })
+
+    describe('ngOnChanges', ()=>{
+      it('Does not calls parsePieChartData if allServiceData does not exist',()=>{
+        comp.appcomponentdata = {};
+        spyOn(comp, "parsePieChartData");
+        comp.ngOnChanges();
+        expect(comp.parsePieChartData).not.toHaveBeenCalled();
+      })
+
+      it('Calls parsePieChartData if allServiceData exist',()=>{
+        comp.appcomponentdata = { allServiceData: true }
+        spyOn(comp, "parsePieChartData");
+        comp.ngOnChanges();
+        expect(comp.parsePieChartData).toHaveBeenCalled();
+      })
     })
 })
