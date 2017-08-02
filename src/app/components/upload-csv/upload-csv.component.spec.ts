2ee5e1829e57144d568cd6b61e5d40b600c09c4a
nitin.gupta1 <nitin.gupta1@3pillarglobal.com>
2017-08-02 15:57:01 +0530

    update spec.ts

--- src/app/components/upload-csv/upload-csv.component.spec.ts
+++ src/app/components/upload-csv/upload-csv.component.spec.ts
@@ -4,19 +4,27 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { UploadCsvComponent } from './upload-csv.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { LoaderComponent } from '../loader/loader.component';
+import { FileUploader } from 'ng2-file-upload';
+import { AwsdataService } from './../../services/awsdata.service';
 
 describe('UploadCsvComponent', () => {
   let component: UploadCsvComponent;
   let fixture: ComponentFixture<UploadCsvComponent>;
+  let awsdataServiceStub = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UploadCsvComponent ]
+        declarations: [UploadCsvComponent, HeaderComponent, FooterComponent, LoaderComponent ],
+        providers: [{ provide: AwsdataService, useValue: awsdataServiceStub }]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+     
     fixture = TestBed.createComponent(UploadCsvComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
